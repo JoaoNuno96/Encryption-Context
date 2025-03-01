4d53b4adef374308a4fb45d2379f09f9d4c7933b
JoaoNuno96 <joaonuno1996@hotmail.com>
2025-03-01 15:02:01 +0000

    First commit, begining to construct logic behind encrypt text

---
+++ Encryption/Entities/Phrase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Encryption.Interface;
+
+namespace Encryption.Entities
+{
+    public class Phrase : ICheck
+    {
+        public string Text { get; set; }
+        public Phrase() { }
+        public Phrase(string text)
+        {
+            this.Text = text;
+        }
+
+        public String RemoveWhiteSpaces(String phrase)
+        {
+            string letter = String.Empty;
+            string newPhrase = String.Empty;
+
+            if(phrase == null)
+            {
+                throw new ArgumentNullException("No Argument given");
+            }
+            else
+            {
+                foreach (char l in phrase)
+                {
+                    if (l == ' ')
+                    {
+                        letter = String.Empty;
+                    }
+                    else
+                    {
+                        letter = l.ToString();
+                    }
+
+                    newPhrase += letter;
+                }
+
+                return newPhrase;
+            }
+
+        }
+
+    }
+}
